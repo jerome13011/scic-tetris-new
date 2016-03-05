10518f5d09b1f6499631e495993d03cb9a92624e
[jerome13011] <[fanniojerome@free.fr]>
2016-03-05 20:19:08 +0100

    ajout js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-
+$(document).ready(function() {
+ // hides the slickbox as soon as the DOM is ready
+$('#description').hide();
+ // shows the slickbox on clicking the noted link
+$('#inclick').click(function() {
+$('#description').show('slow');
+return false;
+  });
+ // hides the slickbox on clicking the noted link
+ $('#onclick').click(function() {
+ $('#description').hide('slow');
+ return false;
+ });
+ // toggles the slickbox on clicking the noted link
+ });
