10ca28841b52dd06983d870b65804be36fb10654
[jerome13011] <[fanniojerome@free.fr]>
2016-03-07 16:28:16 +0100

    ajout scrol nav

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 $('#description').hide();
  // shows the slickbox on clicking the noted link
 $('#inclick').click(function() {
-$('#description').show('slow');
+$('#description').toggle('slow');
 return false;
   });
  // hides the slickbox on clicking the noted link
@@ -12,4 +12,17 @@ return false;
  return false;
  });
  // toggles the slickbox on clicking the noted link
+
+ $('#navh').hide();
+  // shows the slickbox on clicking the noted link
+ $('#clic').click(function() {
+ $('#navh').toggle('explode');
+ return false;
+   });
+  // hides the slickbox on clicking the noted link
+  $('#X').click(function() {
+  $('#navh').hide('explode');
+  return false;
+  });
+  // toggles the slickbox on clicking the noted link
  });
